77edfb3eb0e861092f0998b4dfb25d369c2964bb
Wesley Andrade <wandrade@gm5.salajustica>
2019-01-17 12:33:21 -0200

    - Correçao de bugs; - Menu mobile;

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,8 +7,34 @@ $(document).ready(function () {
         $('.tendencias-carousel .slick-arrow').wrapAll('<div class="tendencias-carousel-nav"/>');
     }
     $( ".hamburger" ).click(function() {
-        $(this).toggleClass('is-active');
+        $(this).toggleClass('is-active'),
+        $('.navigation:not(.-mobile').css('z-index', 20),
+        $('.navigation.-mobile').toggleClass('toggle');
     }); 
+    $(window).on('resize scroll', function () {
+        $('.is-active').removeClass('is-active'),
+        $('.toggle').removeClass('toggle');
+    });     
+    $('.navigation.-mobile a').each(function() {
+        $(this).click(function() {
+            if($(this).next('ul').length)
+            {
+                $(this).toggleClass('toggle'),
+                $(this).next('ul').toggleClass('toggle');
+            }
+        }); 
+    });    
+    $(document).mouseup(function (e)
+    {
+        var container = $('.navigation.-mobile').children();
+
+        if (!container.is(e.target) 
+            && container.has(e.target).length === 0)
+        {
+            $('.is-active').removeClass('is-active'),
+            $('.toggle').removeClass('toggle');
+        }
+    });        
 });
       
       
